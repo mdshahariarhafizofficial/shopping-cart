c12d65c56daedb734b4895afeff5fd28e87c1d19
MdShahariarHafiz <shahariarwebdev@gmail.com>
2023-08-18 11:53:48 +0600

    cart price updated

--- shop.js
+++ shop.js
@@ -1,18 +1,21 @@
-const plusBtn = document.getElementById('plus-btn');
-plusBtn.addEventListener('click', function(){
-    const inputValue = document.getElementById('input-value').value;
-    const inputValueNumber = parseFloat(inputValue);
+// const plusBtn = document.getElementById('plus-btn');
+// plusBtn.addEventListener('click', function(){
+//     const inputValue = document.getElementById('input-value').value;
+//     const inputValueNumber = parseFloat(inputValue);
 
-    const totalValue = inputValueNumber+1
-    document.getElementById('input-value').value = totalValue;
+//     const totalValue = inputValueNumber+1
+//     document.getElementById('input-value').value = totalValue;
 
     
-    const ipPrice = document.getElementById('ip-price').innerText;
-    const ipPriceNumber = parseFloat(ipPrice);
-    const totalIpPrice = ipPriceNumber*totalValue;
-    document.getElementById('ip-price').innerText = totalIpPrice;
+//     const ipPrice = document.getElementById('ip-price').innerText;
+//     // const ipPriceNumber = parseFloat(ipPrice);
+//     const totalIpPrice = totalValue*1219;
+//     document.getElementById('ip-price').innerText = totalIpPrice;
 
-})
+// })
+
+itemAddFun('plus-btn', 1219)
+
 
 const minusBtn = document.getElementById('minus-btn');
 minusBtn.addEventListener('click', function(){
@@ -23,4 +26,28 @@ minusBtn.addEventListener('click', function(){
         return 0;
     }
     document.getElementById('input-value').value = totalNegativeValue;
+
+
+    const ipPrice = document.getElementById('ip-price').innerText;
+    const totalIpPrice = totalNegativeValue*1219;
+    document.getElementById('ip-price').innerText = totalIpPrice;
+
 })
+
+function itemAddFun(id, price) {
+    const plusBtn = document.getElementById(id);
+    plusBtn.addEventListener('click', function(){
+    const inputValue = document.getElementById('input-value').value;
+    const inputValueNumber = parseFloat(inputValue);
+
+    const totalValue = inputValueNumber+1
+    document.getElementById('input-value').value = totalValue;
+
+    
+    const ipPrice = document.getElementById('ip-price').innerText;
+    // const ipPriceNumber = parseFloat(ipPrice);
+    const totalIpPrice = totalValue*price;
+    document.getElementById('ip-price').innerText = totalIpPrice;
+
+})
+}
